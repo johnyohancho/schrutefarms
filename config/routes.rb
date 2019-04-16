836060e1fa634ad19d888bcb7966e824c310b261
github username <github email address>
2019-04-16 13:31:54 -0400

    login page and session funtionality built.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'root#home'
+
+  get '/items/BnB', to: 'items#index_BnB', as: 'BnB_items'
+  get '/items/store', to: 'items#index_store', as: 'store_items'
   resources :users, except: :index
-  resources :items, only: [:index, :show]
+  resources :items, only: :show
   resources :orders, only: [:index, :new, :create]
   resources :reviews, only: [:new, :create, :edit, :update]
 
@@ -11,4 +14,5 @@ Rails.application.routes.draw do
   delete '/login', to: 'sessions#destroy'
 
   get '/about', to: 'about#about', as: 'about'
+
 end
