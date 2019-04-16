2e23192250ba6d1d872ecfaca7534994cf6f3b55
Ansara Hollinger <ansara.hooks@gmail.com>
2019-04-15 21:47:15 -0400

    added controllers and views for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, except: :index
+  root 'users#index', as: 'home'
+  resources :users
   resources :items, only: [:index, :show]
   resources :orders, only: [:index, :new, :create]
   resources :reviews, only: [:new, :create, :edit, :update]
