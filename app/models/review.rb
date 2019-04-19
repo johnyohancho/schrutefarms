3b580155621daafad2febd23dce9c1def12e7d12
github username <github email address>
2019-04-18 22:45:40 -0400

    could not figure out edit reviews. worked on some html and css. signed in feature added.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :content, presence: true
+  validates :content, length: { maximum: 140 }
+  validates :rating, presence: true
 end
